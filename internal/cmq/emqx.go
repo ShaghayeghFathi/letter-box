0f8324bb2bec3cc09f0a5d729e9e3f45fa3d7827
shaghayegh.fathi <sh.fathi@snapp.cab>
2023-04-28 19:40:44 +0330

    add nats streaming to server

--- internal/cmq/emqx.go
+++ internal/cmq/emqx.go
@@ -1,1 +1,6 @@
 package cmq
+
+type CMQ interface {
+	QueueSubscribe(subject, qg string, handler func(subject string, data []byte))
+	UnsubscribeAll()
+}
--- server.go
+++ server.go
@@ -1,8 +1,18 @@
 package letter_box
 
+import (
+	"github.com/ShaghayeghFathi/letter-box/internal/cmq"
+	"github.com/ShaghayeghFathi/letter-box/internal/config"
+)
+
 type ServerConfig struct {
 	Name string
 	URL  string
+}
+
+type Server struct {
+	//Emq EMQ
+	NatsStreaming *cmq.CMQ
 }
 
 func NewServer(name string, url string) {
@@ -10,6 +20,16 @@ func NewServer(name string, url string) {
 	case "emqx":
 		{
 			// setup emqx connection
+		}
+	case "nats":
+		{
+			c := config.NatsStreaming{
+				Address:   "",
+				ClusterID: "",
+				ClientID:  "",
+			}
+			cmq.Connect(c)
+
 		}
 
 	default:
